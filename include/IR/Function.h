ac58e020f3ca1db09622f709f723b3f44ff41043
Zhu Enzuo <misakihanayo@qq.com>
2021-07-16 14:34:40 +0800

    Function.h,.cpp and Instruction.h

---
+++ include/IR/Function.h
@@ -0,0 +1,39 @@
+#include "Module.h"
+#include "Type.h"
+#include "BasicBlock.h"
+#include <iterator>
+#include <list>
+#include <cstddef>
+#include <map>
+#include <cassert>
+
+class Module;
+class Argument;
+class BasicBlock;
+class Type;
+class FunctionType;
+
+class Function: public Value{
+public:
+    Function (FunctionType* type, const std::string &name, Module *parent);
+    ~Function();
+    static Function *create_function(const std::string &id, Module *m, FunctionType *type, vector args_);
+    FunctionType *get_function_type() const;
+    Type *get_return_type() const;
+    void add_basic_block(BasicBlock *bb);
+    unsigned get_num_of_args() const;
+    unsigned get_num_basic_blocks() const;
+    Module *get_parents() const;
+    std::list<Argument *>::iterator arg_begin();
+    std::list<Argument *>::iterator arg_end();
+    void remove(BasicBlock *bb);
+    std::list<BasicBlock *> &get_basic_blocks();
+    std::list<Argument *> &get_args();
+    void set_instr_name();
+private:
+    FunctionType *type;
+    Module *parent;
+    std::vector<Value*> arguments;
+    std::list<BasicBlock *> basic_block;
+    std::string *id;
+}
