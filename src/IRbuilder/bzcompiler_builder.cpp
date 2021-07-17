4191667043b6d061bd12babb9deb87ff666cdcce
Zhu Enzuo <misakihanayo@qq.com>
2021-07-17 16:45:37 +0800

    add builder.cpp

---
+++ src/IRbuilder/bzcompiler_builder.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Misakihanayo on 2021/7/17.
+//
+
+#include "bzcompiler_builder.hpp"
+void ASTvisitor::visit(ASTProgram &) {}
+void ASTvisitor::visit(ASTConstant &) {}
+void ASTvisitor::visit(ASTUnaryOp &) {}
+void ASTvisitor::visit(ASTMulOp &) {}
+void ASTvisitor::visit(ASTAddOp &) {}
+void ASTvisitor::visit(ASTRelOp &) {}
+void ASTvisitor::visit(ASTEqOp &) {}
+void ASTvisitor::visit(ASTAndOp &) {}
+void ASTvisitor::visit(ASTOrOp &) {}
+void ASTvisitor::visit(ASTLVal &) {}
+void ASTvisitor::visit(ASTFuncCall &) {}
+void ASTvisitor::visit(ASTStatement &) {}
+void ASTvisitor::visit(ASTDecl &) {}
+void ASTvisitor::visit(ASTVarDecl &) {}
+void ASTvisitor::visit(ASTAssignStmt &) {}
+void ASTvisitor::visit(ASTExpressionStmt &) {}
+void ASTvisitor::visit(ASTIfStmt &) {}
+void ASTvisitor::visit(ASTWhileStmt &) {}
+void ASTvisitor::visit(ASTBreakStmtStmt &) {}
+void ASTvisitor::visit(ASTContinueStmtStmt &) {}
