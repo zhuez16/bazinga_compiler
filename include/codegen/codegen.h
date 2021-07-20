fd661a30cce2d37042e69cbdfd656b32a946fe80
Enzo Zhu <misakihanayo@qq.com>
2021-07-20 19:56:18 +0800

    initial codegen

---
+++ include/codegen/codegen.h
@@ -0,0 +1,14 @@
+//
+// Created by mskhana on 2021/7/20.
+//
+
+#ifndef BAZINGA_COMPILER_CODEGEN_H
+#define BAZINGA_COMPILER_CODEGEN_H
+
+
+class codegen {
+
+};
+
+
+#endif //BAZINGA_COMPILER_CODEGEN_H
---
+++ src/codegen/codegen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by mskhana on 2021/7/20.
+//
+
+#include "include/codegen/codegen.h"
