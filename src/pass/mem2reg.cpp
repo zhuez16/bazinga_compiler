88da595f283509682b812806957ecf64ea8e5f4e
xzwj1699 <xzwj@ustc.edu.cn>
2021-07-21 17:36:22 +0800

    fix mem2reg

--- src/pass/mem2reg.cpp
+++ src/pass/mem2reg.cpp
@@ -1,7 +1,10 @@
 #include <IR/BasicBlock.h>
-#include "Pass/mem2reg.h"
-#include "Pass/dominator.h"
-void Mem2RegPass::run() {
+#include "pass/mem2reg.h"
+#include "pass/dominator.h"
+
+std::map<Value *, std::vector<Value *>> var_val_stack;
+
+void Mem2Reg::run() {
     printf("running mem2reg\n");
     for(auto f: m_->get_functions()){
         // get dominance frontier message
@@ -52,6 +55,80 @@ void Mem2RegPass::run() {
                 }
             }
         }
+        re_name(f->get_entry_block());
+    }
+}
+void Mem2Reg::re_name(BasicBlock *bb) {
+    std::vector<Instruction *> wait_delete;
+    for (auto instr : bb->get_instructions() ){
+        if (instr->is_phi()){
+            // step 3: push phi instr as lval's lastest value define
+            auto l_val = dynamic_cast<PhiInst *>(instr)->get_lval();
+            var_val_stack[l_val].push_back(instr);
+        }
+    }
 
+    for (auto instr : bb->get_instructions() ){
+        if ( instr->is_load() ){
+            // step 4: replace load with the top of stack[l_val]
+            auto l_val = dynamic_cast<LoadInst *>(instr)->get_lval();
+
+            if (!dynamic_cast<GlobalVariable *>(l_val) &&
+                !dynamic_cast<GetElementPtrInst *>(l_val)){
+                if ( var_val_stack.find(l_val)!=var_val_stack.end()){
+                    instr->replace_all_use_with(var_val_stack[l_val].back());
+                    wait_delete.push_back(instr);
+                }
+            }
+        }
+        if (instr->is_store()){
+            // step 5: push r_val of store instr as lval's lastest definition
+            auto l_val = dynamic_cast<StoreInst *>(instr)->get_lval();
+            auto r_val = dynamic_cast<StoreInst *>(instr)->get_rval();
+
+            if (!dynamic_cast<GlobalVariable *>(l_val) &&
+                !dynamic_cast<GetElementPtrInst *>(l_val)){
+                var_val_stack[l_val].push_back(r_val);
+                wait_delete.push_back(instr);
+            }
+        }
+    }
+
+    for ( auto succ_bb : bb->get_succ_basic_blocks() ){
+        for ( auto instr : succ_bb->get_instructions() ){
+            if ( instr->is_phi()){
+                auto l_val = dynamic_cast<PhiInst *>(instr)->get_lval();
+                if (var_val_stack.find(l_val)!= var_val_stack.end()){
+//                    assert(var_val_stack[l_val].size()!=0);
+                    // step 6: fill phi pair parameters
+                    dynamic_cast<PhiInst *>(instr)->add_phi_pair_operand( var_val_stack[l_val].back(), bb);
+                }
+                // else phi parameter is [ undef, bb ]
+            }
+        }
+    }
+
+    for ( auto dom_succ_bb : dom->get_dom_tree_succ_blocks(bb) ){
+        re_name(dom_succ_bb);
+    }
+
+    for (auto instr : bb->get_instructions()){
+        // step 7: pop lval's lastest definition
+        if(instr->is_store()){
+            auto l_val = dynamic_cast<StoreInst *>(instr)->get_lval();
+            if (!dynamic_cast<GlobalVariable *>(l_val) &&
+                !dynamic_cast<GetElementPtrInst *>(l_val)){
+                var_val_stack[l_val].pop_back();
+            }
+        }
+        else if (instr->is_phi()){
+            auto l_val = dynamic_cast<PhiInst *>(instr)->get_lval();
+            if ( var_val_stack.find(l_val)!=var_val_stack.end()){
+                var_val_stack[l_val].pop_back();
+            }
+        }
+    }
+    for ( auto instr : wait_delete){
+        bb->delete_instr(instr);
     }
 }
