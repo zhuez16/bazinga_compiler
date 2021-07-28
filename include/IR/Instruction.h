dc4ac57f5a1017e79d8d8df1bd0d10b2b285eba8
guch8017 <guch8017@gmail.com>
2021-07-28 11:09:05 +0800

    Finish the algo for finding loop infer variables

--- include/IR/Instruction.h
+++ include/IR/Instruction.h
@@ -559,6 +559,7 @@ private:
 
 public:
     static PhiInst *create_phi(Type *ty, BasicBlock *bb);
+    std::list<std::pair<Value *, BasicBlock *>> getValueBBPair();
     Value *get_lval() { return l_val_; }
     void set_lval(Value *l_val) { l_val_ = l_val; }
     void add_phi_pair_operand(Value *val, Value *pre_bb)
